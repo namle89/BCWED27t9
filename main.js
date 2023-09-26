945ab8919ba22f4bb4eded857f8141b8fa2f377d
namle89 <lehoangnam89@gail.com>
2023-09-26 18:55:39 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+function to_open_VD1()
+{
+    window.location.href = "/VD1.html"
+}
+
+function to_open_VD2()
+{
+    window.location.href = "/VD2.html"
+}
+
+function closeWebsite() {
+    window.close();
+}
